d2bee5d3149be00fd98c4d7cc2ba5d0d890e9244
Austin Parker <austincparker@gmail.com>
2022-02-01 18:41:25 -0600

    store and ass

---
+++ greyjoy-quicktrippin/Models/Associate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace greyjoy_quicktrippin.Models
+{
+    internal class Associate : Employee
+    {
+        public double RetailSales { get; set; }
+        public double GasSales { get; set; }
+        public Associate(string name, int empNum, double retail, double gas)
+        {
+            Name = name;
+            EmployeeNum = empNum;
+            RetailSales = retail;
+            GasSales = gas;
+        }
+    }
+}
