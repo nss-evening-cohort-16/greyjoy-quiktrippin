82ef9909762ceb7bbc69003832b4d294ead5cfa9
Joe Gibbs <joseph.gibbs1188@gmail.com>
2022-02-01 18:45:06 -0600

    District/Employee edit

---
+++ greyjoy-quicktrippin/Models/District.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace greyjoy_quicktrippin.Models
+{
+    internal class District
+    {
+        public List<Store> StoresList = new List<Store>();
+        public int DistrictNumber { get; set; }
+        public double DistrictTotalRetail { get; set; }
+        public double DistrictTotalGas { get; set; }
+
+        public void AddStore(Store store);
+            Store.Add(store);
+
+        public void DistrictGasTotal();
+
+        public void DistrictRetailTotal();
+    }
+}
